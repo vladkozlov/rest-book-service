673ae9957f9d380a30e67814b2817199bbbc0cc0
vkozlov <v-vkozlov@hotels.com>
2020-12-14 14:39:27 +0100

    init

---
+++ src/main/java/com/epam/restbookservice/RestBookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.epam.restbookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestBookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestBookServiceApplication.class, args);
+	}
+
+}
