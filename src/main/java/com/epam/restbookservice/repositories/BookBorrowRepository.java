a993c5b8d7e1064b2cf38afc73b4c00d15d3b37e
zerogclub <zerogclub@gmail.com>
2021-01-27 18:23:02 +0100

    fixing multiple sql queries when fetching

--- src/main/java/com/epam/restbookservice/repositories/BookBorrowRepository.java
+++ src/main/java/com/epam/restbookservice/repositories/BookBorrowRepository.java
@@ -2,6 +2,7 @@ package com.epam.restbookservice.repositories;
 
 import com.epam.restbookservice.domain.Book;
 import com.epam.restbookservice.domain.BookBorrow;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,6 +13,11 @@ import java.util.List;
 @Repository
 public interface BookBorrowRepository extends JpaRepository<BookBorrow, Long> {
 
+    @EntityGraph(
+            type = EntityGraph.EntityGraphType.LOAD,
+            attributePaths = {"book"}
+    )
+    List<BookBorrow> findAll();
     boolean existsBookBorrowByBook(Book book);
     @Query("select bb from BookBorrow bb join fetch bb.user where bb.expireAt = :date")
     List<BookBorrow> findAllWithExpiringDateEqual(LocalDate date);
--- src/main/java/com/epam/restbookservice/repositories/UserRepository.java
+++ src/main/java/com/epam/restbookservice/repositories/UserRepository.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+//    @Query("select u from User u  join fetch u.roles where u.username = :name")
     Optional<User> findByUsername(String name);
 
     boolean existsByUsername(String username);
