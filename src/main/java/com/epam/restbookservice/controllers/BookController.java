8697ecc4e176650e29cc72beaa81ed876ea70d90
zerogclub <zerogclub@gmail.com>
2021-01-29 00:09:41 +0100

    DTO mapper now used from BookBorrowDTO

--- src/main/java/com/epam/restbookservice/controllers/BookController.java
+++ src/main/java/com/epam/restbookservice/controllers/BookController.java
@@ -1,15 +1,6 @@
 package com.epam.restbookservice.controllers;
 
-import static java.lang.String.format;
-
-import java.net.URI;
-import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import com.epam.restbookservice.domain.Book;
-import com.epam.restbookservice.domain.BookBorrow;
 import com.epam.restbookservice.dtos.BookBorrowDTO;
 import com.epam.restbookservice.dtos.BookDTO;
 import com.epam.restbookservice.exceptions.ExpiryDateInvalidException;
@@ -23,8 +14,26 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.net.URI;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.lang.String.format;
 
 @RestController
 @RequestMapping("/books")
@@ -127,17 +136,8 @@ public class BookController implements SecuredController {
     public List<BookBorrowDTO> getBorrowedBooks() {
         return bookBorrowService.getAllBorrows()
                 .stream()
-                .map(this::bookBorrowToBookBorrowDTO)
+                .map(BookBorrowDTO::bookBorrowToBookBorrowDTO)
                 .collect(Collectors.toList());
-    }
-
-    private BookBorrowDTO bookBorrowToBookBorrowDTO(BookBorrow bookBorrow) {
-        return BookBorrowDTO.builder()
-                .id(bookBorrow.getBook().getId())
-                .title(bookBorrow.getBook().getTitle())
-                .isbn(bookBorrow.getBook().getISBN())
-                .tillDate(bookBorrow.getExpireAt())
-                .build();
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
